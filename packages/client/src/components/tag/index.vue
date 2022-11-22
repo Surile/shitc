ad823eff83dadbc15fe227e5423000bde47244f0
夜雨 <mhecy98@gmail.com>
2022-11-23 00:05:50 +0800

    feat: 商品详情

---
+++ packages/client/src/components/tag/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view :class="[$style.tag, 'font-medium']">{{ props.name }}</view>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  name: string
+}>()
+</script>
+<style module lang="scss">
+.tag {
+  padding: 6rpx 16rpx;
+  display: inline-block;
+  color: $text-color;
+  font-size: 24rpx;
+  background-color: #f7f7f7;
+}
+</style>
